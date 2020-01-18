9361d6c596d37b3b29e9c7fa3f5d3311b29ffa6b
Maciej Skrabski <maciej.skrabski@gmail.com>
2020-01-19 00:48:51 +0100

    Button

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.tutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        var add = findViewById<Button>(R.id.buttonAdd)
+        add.setOnClickListener { Toast.makeText(this, "Guzik!", Toast.LENGTH_SHORT).show() }
     }
 }
