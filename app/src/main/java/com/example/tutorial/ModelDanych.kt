a423d91c77dc673750d52024d47f7eda7379bed2
Maciej Skrabski <maciej.skrabski@gmail.com>
2020-01-19 04:01:11 +0100

    Data Classes compatible with project idea

--- app/src/main/java/com/example/tutorial/ModelDanych.kt
+++ app/src/main/java/com/example/tutorial/ModelDanych.kt
@@ -1,11 +1,33 @@
 package com.example.tutorial
 
+import androidx.room.Embedded
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import java.io.StringBufferInputStream
 
 @Entity
 data class Danie (
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true) val id: Int,
     val name: String,
     val desc: String
+)
+
+@Entity(tableName = "osoby")
+data class Osoba (
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val imie: String,
+    val nazwisko: String
+
+)
+
+@Entity(tableName = "ogloszenia", foreignKeys = [ForeignKey(entity = Osoba::class,
+    parentColumns = arrayOf("id"),
+    childColumns = arrayOf("autor"),
+    onDelete = ForeignKey.CASCADE)])
+data class Ogloszenie (
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val tytul: String,
+    val tresc: String,
+    val autor: Int
 )
