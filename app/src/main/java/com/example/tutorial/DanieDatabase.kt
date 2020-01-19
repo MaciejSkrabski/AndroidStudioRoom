6f94c6ff7f82e61d3df1553014a715c83ea42921
Maciej Skrabski <maciej.skrabski@gmail.com>
2020-01-19 02:09:23 +0100

    hahaha działa

--- app/src/main/java/com/example/tutorial/DanieDatabase.kt
+++ app/src/main/java/com/example/tutorial/DanieDatabase.kt
@@ -3,7 +3,7 @@ package com.example.tutorial
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Danie::class], version = 0)
+@Database(entities = [Danie::class], version = 1) //Musi być >= 1
 abstract class DanieDatabase: RoomDatabase(){
     abstract fun danieDao(): IDanie
 }
--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.tutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log.d
 import android.widget.Button
 import android.widget.Toast
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +19,25 @@ class MainActivity : AppCompatActivity() {
         val db = Room.databaseBuilder(
             this, DanieDatabase::class.java,"meal_database"
         )
+            .allowMainThreadQueries()
+            .build()
+
+        val wszystkieDania = db.danieDao().pobierzWszystkieDania()
+
+        wszystkieDania.forEach{
+            textView.append("\n id: ${it.id}, nazwa:${it.name}, opis:${it.desc}")
+        }
+        d("dania", "ile dań? ${wszystkieDania.size}")
+
+
         var add = findViewById<Button>(R.id.buttonAdd)
-        add.setOnClickListener { Toast.makeText(this, "Guzik!", Toast.LENGTH_SHORT).show() }
+        add.setOnClickListener {
+            Toast.makeText(this, "Guzik!", Toast.LENGTH_SHORT).show()
+            dodaj(db)
+        }
+    }
+
+    fun dodaj(db: DanieDatabase) {
+        db.danieDao().dodajDanie(Danie(name=edit_text_name.text.toString(), desc=edit_text_desc.text.toString()))
     }
 }
